80da2cab6cc71fbf03fb406018023547959bce02
jiwon02971@gmail.com <pm124578369>
2019-12-07 03:05:09 +0900

    commit

---
+++ src/main/java/iKguana/artonline/LogAPI/LogAPI.java
@@ -0,0 +1,36 @@
+package iKguana.artonline.LogAPI;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import cn.nukkit.Server;
+
+public class LogAPI {
+    public static File logFolder;
+
+    public LogAPI() {
+        logFolder = new File(Server.getInstance().getDataPath(), "logs-custom");
+        logFolder.mkdirs();
+    }
+
+    public static void saveLog(String fname, String data) {
+        (new File(logFolder, fname)).mkdirs();
+
+        String yyyyMMdd = new SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(new Date());
+        String kkMM = new SimpleDateFormat("kk:MM", Locale.KOREA).format(new Date());
+
+        File file = new File(logFolder, fname + File.separator + yyyyMMdd + ".txt");
+        try {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)), true);
+            writer.println(kkMM + " " + data);
+            writer.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+}
